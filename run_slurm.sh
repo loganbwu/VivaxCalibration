0c24189901f9ba65678beb20a80913963462574a
Logan Wu <loganbwu@gmail.com>
2024-01-12 21:20:50 +0800

    Switch slurm script to run actual R

--- run_slurm.sh
+++ run_slurm.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=run_method_experiments
 #SBATCH --time=2-00:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=56
 #SBATCH --mem=16GB
 module load R/4.3.2
 
-Rscript --no-save hello_world.R
+Rscript --no-save chapter_01_samplingonly.R
