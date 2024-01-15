c76a948feea94543639045f6c7951fc8e2efed9d
Logan Wu <loganbwu@gmail.com>
2024-01-15 11:51:24 +0800

    Test timeout in script

--- R/test.R
+++ R/test.R
@@ -1,13 +1,21 @@
 source("R/constants.R")
 
 myfun = function(x) {
-  Sys.sleep(2)
-  
-  return(x)
+  withTimeout({
+    Sys.sleep(x)
+    
+    return(x)
+  },
+  timeout=2,
+  onTimeout = "warning")
 }
 
-myfun(1)
 
 with_timeout(myfun(1), cpu=1, elapsed=1)
 
 withTimeout(myfun(1.1), timeout=1, onTimeout="warning")
+
+
+sequence = c(1, 3, 5)
+
+results = lapply(sequence, myfun)
