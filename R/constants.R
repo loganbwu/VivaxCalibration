c140add8698919514d7a57985f2de8e1d4ca76b2
Logan Wu <loganbwu@gmail.com>
2024-01-27 11:49:59 +0800

    Redo changes except without model changes

--- R/constants.R
+++ R/constants.R
@@ -20,6 +20,24 @@ renamed_comparison_colors = c("Nonseasonal, poisson" = "tomato",
 rename_methods = names(comparison_colors)
 names(rename_methods) = names(renamed_comparison_colors)
 
+label_auto = function(x) {
+  max_x = max(x, na.rm=T)
+  if (max_x > 5e6) {
+    paste(x/1e6, "M")
+  } else if (max_x > 5e3) {
+    paste(x/1e3, "k")
+  } else {
+    x
+  }
+}
+
+label_auto2 = function(x) {
+  case_when(x >= 1e6 ~ paste(x/1e6, "M"),
+            x >= 1e3 ~ paste(x/1e3, "k"),
+            TRUE ~ as.character(x))
+}
+
+
 # Use this to filter out chains that haven't converged
 .calculate_rhat = function(.trace, .n_chains = n_chains) {
   if (is.null(.trace)) {
