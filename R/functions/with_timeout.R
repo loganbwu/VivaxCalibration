ad0191cf8e7839061f7a3f291d6e737e0cd24df8
Logan Wu <loganbwu@gmail.com>
2024-03-11 09:39:31 +1100

    Prepare for HPC

--- R/functions/with_timeout.R
+++ R/functions/with_timeout.R
@@ -3,7 +3,10 @@
 #' @param cpu maximum time in seconds
 #'
 #' https://stackoverflow.com/a/53018594
-with_timeout <- function(expr, cpu, elapsed){
+with_timeout <- function(expr, cpu = Inf, elapsed = Inf){
+  if (cpu == Inf & elapsed == Inf) {
+    stop("Both cpu and elapsed are Inf. Set at least one.")
+  }
   expr <- substitute(expr)
   envir <- parent.frame()
   setTimeLimit(cpu = cpu, elapsed = elapsed, transient = TRUE)
