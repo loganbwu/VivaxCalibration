2041520d91a571b4600a9d518ca5bc53e5766fdf
Logan Wu <loganbwu@gmail.com>
2024-07-12 14:46:00 +1000

    Make chapter 2 run for 2 weeks on long

--- slurm_chapter_02.sh
+++ slurm_chapter_02.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=chap_02
 #SBATCH --partition=regular
-#SBATCH --time=2-00
-#SBATCH --cpus-per-task=128
+#SBATCH --time=7-00
+#SBATCH --cpus-per-task=96
 #SBATCH --mem=64G
 #SBATCH --ntasks=1
 #SBATCH --mail-user=wu.l@wehi.edu.au
