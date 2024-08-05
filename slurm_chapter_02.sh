e6dd6b8ddd701a675878b6523ede10c15b456193
Logan Wu <loganbwu@gmail.com>
2024-08-05 13:38:07 +1000

    Set to 1.5 days expected

--- slurm_chapter_02.sh
+++ slurm_chapter_02.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=chap_02
 #SBATCH --partition=regular
-#SBATCH --time=1-00
+#SBATCH --time=2-00
 #SBATCH --cpus-per-task=128
-#SBATCH --mem=64G
+#SBATCH --mem=32G
 #SBATCH --ntasks=1
 #SBATCH --mail-user=wu.l@wehi.edu.au
 #SBATCH --mail-type=END
